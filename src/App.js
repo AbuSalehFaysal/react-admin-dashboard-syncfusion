0aba077a2aeffca4168b48233cd738f9e6737caa
AbuSalehFaysal <asfaysal.bracu@gmail.com>
2022-06-11 16:40:24 +0600

    sidebar, navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
 
 import "./App.css";
 
 const App = () => {
+  const activeMenu = true;
   return (
-    <h1 className="underline text-3xl">App</h1>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ backgroundColor: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {activeMenu ? (
+            <div className="w-72 fixed dark:bg-secondary-dark-bg bg-white">
+              SideBar
+            </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg">
+              Not Sidebar
+            </div>
+          )}
+          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg-navbar w-full">
+              Navbar
+            </div>
+          </div>
+          <div>
+            <Routes>
+              <Route path="/" element="ECommerce" />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
